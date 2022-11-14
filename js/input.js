3f8587ef6c8954064bc42682f2e1f0688ab00f5f
Camila López Cesario <camilalopezc777@gmail.com>
2022-11-14 10:18:05 -0300

    Inicio de archivo de JS

---
+++ js/input.js
@@ -0,0 +1,10 @@
+totalAPagar(int cantidad, string categoria){
+    float total;
+    if(categoria == 'Estudiante'){
+        total = cantidad * (200-(80*200/100))
+    }elseif(categoria == 'Trainee'){
+        total = cantidad * (200-(50*200/100))    
+    }elseif(categoria == 'Junior'){
+        total = cantidad * (200-(15*200/100))    
+    }
+}
