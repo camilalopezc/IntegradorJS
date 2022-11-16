fe26c46606b0a22036cbbd08f4e7757120ecb9b9
Camila López Cesario <camilalopezc777@gmail.com>
2022-11-16 16:43:30 -0300

    arreglo detalles JS

--- js/input.js
+++ js/input.js
@@ -1,10 +1,12 @@
-function totalAPagar(int cantidad, string categoria){
-    float total;
+function totalAPagar(cantidad, categoria){
+    let total;
+    total = 0;
     if(categoria == 'Estudiante'){
         total = cantidad * (200-(80*200/100))
-    }elseif(categoria == 'Trainee'){
-        total = cantidad * (200-(50*200/100))    
-    }elseif(categoria == 'Junior'){
-        total = cantidad * (200-(15*200/100))    
+    }else if(categoria == 'Trainee'){
+        total = cantidad * (200-(50*200/100))
+    }else if(categoria == 'Junior'){
+        total = cantidad * (200-(15*200/100))   
     }
+    return total;
 }
