1fcee672a35561dabddc55b1e9e72fc380c04f8e
Camila López Cesario <camilalopezc777@gmail.com>
2022-11-28 14:45:38 -0300

    arreglos js

--- js/input.js
+++ js/input.js
@@ -1,12 +1,33 @@
-function totalAPagar(cantidad, categoria){
+const Cantidad = document.getElementById("cantidad");
+const Categoria = document.getElementById("categoria");
+
+const SendButton = document.getElementById("send-button");
+SendButton.addEventListener("click", totalAPagar);
+
+selectElement.addEventListener("change", showTotal);
+
+function totalAPagar(Cantidad, Categoria){
     let total;
     total = 0;
-    if(categoria == 'Estudiante'){
-        total = cantidad * (200-(80*200/100))
-    }else if(categoria == 'Trainee'){
-        total = cantidad * (200-(50*200/100))
-    }else if(categoria == 'Junior'){
-        total = cantidad * (200-(15*200/100))   
+    if(Categoria == 'Estudiante'){
+        total = Cantidad * (200-(80*200/100))
+    }else if(Categoria == 'Trainee'){
+        total = Cantidad * (200-(50*200/100))
+    }else if(Categoria == 'Junior'){
+        total = Cantidad * (200-(15*200/100))   
+    }else{
+        total = Cantidad * 200
     }
-    return total;
+    return total;    
+}
+
+function showTotal(){
+    if(selectElement.value){
+        
+    }
+    const element = document.createElement("div");
+    element.className("card");
+    element.innerHTML = '<p><strong>Total a pagar:$ {producto.getTotal()}</strong></p>';
+
+    container.appendChild(element);
 }
